4064ca7f80c4f8ae069c4c8b612efb6fecb59787
Erisnilton Freitas <erisniltongf@gmail.com>
2022-02-04 16:48:51 -0300

    Terceiro Exemplo

---
+++ Nextjs/exercicios/pages/basicos/segundo.js
@@ -0,0 +1,11 @@
+export default function segundo() {
+  return (
+    <>
+    <div className="container text-center">
+      <h4 className="text-center">Segundo Exemplo!</h4>
+      <button>Segundo botão</button>
+
+    </div>
+    </>
+  );
+}
